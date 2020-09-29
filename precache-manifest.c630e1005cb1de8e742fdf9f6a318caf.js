850b843dd7a664584204f23895d7dfeac1badb3e
Arpit-theBot <Arpitrao2000@gmail.com>
2020-09-29 11:28:53 +0530

    Updates

---
+++ precache-manifest.c630e1005cb1de8e742fdf9f6a318caf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8231ce2ce0fe26ca56eb7c7a884ffd",
+    "url": "/To-Do-APP/index.html"
+  },
+  {
+    "revision": "80f7f52fd502d6fabb6c",
+    "url": "/To-Do-APP/static/css/main.b34d4b17.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/To-Do-APP/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-APP/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f7f52fd502d6fabb6c",
+    "url": "/To-Do-APP/static/js/main.5dc468e5.chunk.js"
+  },
+  {
+    "revision": "233da5a4652d3f594255",
+    "url": "/To-Do-APP/static/js/runtime-main.770a219b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-APP/static/media/logo.5d5d9eef.svg"
+  }
+]);
